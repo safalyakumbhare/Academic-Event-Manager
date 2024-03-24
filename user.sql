5ee6b650b9f3492b338ec5eb9bee9c0c44eb5829
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-03-25 00:08:13 +0530

    Database Updated

--- user.sql
+++ user.sql
@@ -1,8 +1,10 @@
 -- Database name :- database1
 
 CREATE TABLE IF NOT EXISTS `login`(
+    `name` VARCHAR(30),
+    `dept` VARCHAR(30),
     `username` VARCHAR(30),
     `password` VARCHAR(30)
 );
 
-INSERT INTO `login` (`username`, `password`) VALUES ('Dhiraj', 'Dhiraj11');
+INSERT INTO `login` (`name`,`dept`,`username`, `password`) VALUES ('Dr Vivek Kapoor','Ghriet','admin', 'admin111');
