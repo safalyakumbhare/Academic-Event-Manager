b4a1fc276156e189247f1e67e8ec8011b017cae0
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-03-24 19:22:46 +0530

    Admin page

--- Adminhome.php
+++ Adminhome.php
@@ -1,61 +1,53 @@
+<?php
+include ("header.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="main.css">
+    <title>Admin Page</title>
 </head>
+<style>
+    .content {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 88.2vh;
+        width: 100vw;
+        background-color: aqua;
+
+    }
+
+    .container {
+
+
+    }
+
+    .box1,
+    .box2,
+    .box3 {
+        height: 25vh;
+        width: 35vw;
+        background-color: blue;
+    }
+</style>
+
 <body>
-    <div class="Task">
-        <nav>
-            <Ul class="heading">
-                <Li class="head">Acadmic event managment</Li>
-            </Ul>
-            <ul>
-                
-            <li><a href="#">Principal</a></li>
-            <li><a href="#">HoD login</a></li>
-            <li><a href="#">Faculty</a></li>
-       
-            <img src="icons8-menu-50.png" class="dot" onclick="toggleMenu()">
-        </ul>
-        <div class="sub-menu-wrap" id="submenu">
-            <div class="sub-menu">
-                <div class="user info">
-
-                </div>
-                <a href="#" class="menu-link">
-                    <img src="icons8-login-50.png">
-                    <P>Login</P>
-                    <span>></span>
-                </a>
-                <a href="#"  class="menu-link">
-                    <img src="icons8-services-30.png">
-                    <P>Services</P>
-                    <span>></span>
-                </a>
-                <a href="#"  class="menu-link">
-                    <img src="icons8-about-24.png">
-                    <P>About us</P>
-                    <span>></span>
-                </a>
-                <a href="#" class="menu-link" >
-                    <img src="icons8-phone-50.png">
-                    <P>Contact us</P>
-                    <span>></span>
-                </a>
-            </div>
+    <div class="content">
+        <div class="container">
+            <a href="">
+                <div class="box1"></div>
+            </a><a href="">
+                <div class="box2"></div>
+            </a><a href="">
+                <div class="box3"></div>
+            </a>
         </div>
-
-        </nav>
     </div>
-    <script>
-        let submenu=document.getElementById("submenu");
-        function toggleMenu(){
-            submenu.classList.toggle("open-menu");
-        }
-    </script>
-    
 </body>
+
 </html>
