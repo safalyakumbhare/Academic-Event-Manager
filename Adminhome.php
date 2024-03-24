efb1223e0590cc2c9391e5012e0c2089e5bbef08
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-03-24 23:21:23 +0530

    Faculty Registration Added

--- Adminhome.php
+++ Adminhome.php
@@ -13,12 +13,13 @@ include ("header.php");
 </head>
 <style>
     .content {
+
         display: flex;
         align-items: center;
         justify-content: center;
         height: 88.2vh;
         width: 100vw;
-        background-color: aqua;
+        background-color: aliceblue;
 
     }
 
@@ -31,23 +32,45 @@ include ("header.php");
     .box1,
     .box2,
     .box3 {
+        border: 1px solid lightgray;
+        background-color: aliceblue;
+        color: black;
         height: 35vh;
         width: 25vw;
         margin: 1vw;
-        display:flex;
+        display: flex;
+        flex-direction: column;
         align-items: center;
-        justify-content: center;
-        background-color: blue;
+        justify-content: space-around;
+    }
+
+    p {
+        color: #000;
     }
 </style>
 
 <body>
     <div class="content">
         <div class="container">
-            <a href="New Hod.php"><div class="box1"><h2>New Hod</h2></div></a>
-            <a href="New Faculty.php"><div class="box2"><h2>New Faculty</h2></div></a>
-            <a href=""><div class="box3"><h2>Show Events</h2></div></a>
-            
+            <a href="New Hod.php">
+                <div class="box1">
+                    <h2>New Hod</h2>
+                    <p>Get Registered Your New Hod</p>
+                </div>
+            </a>
+            <a href="New Faculty.php">
+                <div class="box2">
+                    <h2>New Faculty</h2>
+                    <p>Get Registered Your New Hod</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box3">
+                    <h2>Show Events</h2>
+                    <p>See Status and Upcoming Events.</p>
+                </div>
+            </a>
+
         </div>
     </div>
 </body>
