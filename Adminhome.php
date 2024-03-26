6c03058ab6c30db74e226846ca94c5ec0414b027
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-03-26 17:23:56 +0530

    Addeing animation in admin page

--- Adminhome.php
+++ Adminhome.php
@@ -4,48 +4,61 @@ include ("header.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="button.css">
-    <title>Admin Home Page</title>
-   
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="button.css">
+  <title>Admin Home Page</title>
+
 </head>
+
 <body>
-    <!-- <nav>
+  <!-- <nav>
         <h1>Acadmeic event managment</h1>
         <ul>
             <li>Welcome admin</li>
         </ul>
     </nav> -->
-<div class="container">
+  <div class="container">
+
     <button class="bttn">
-    <div class="box hover-box" id="new-hod">
-      <div class="content">
-        <h2>New Hod</h2>
-        <p>Get Registered</p>
-        
+      <a href="New Hod.php">
+        <div class="box hover-box" id="new-hod">
+
+          <div class="content">
+
+            <h2>New Hod</h2>
+            <p>Get Register Your New HOD</p>
+
+          </div>
+
+        </div>
+      </a>
+    </button>
+
+    <button class="bttn">
+      <a href="New Faculty.php">
+        <div class="box hover-box" id="new-faculty">
+          <div class="content">
+
+            <h2>New Faculty</h2>
+            <p>Get Register Your New Faculty</p>
+
+          </div>
+        </div>
+      </a>
+    </button>
+    <button class="bttn">
+      <div class="box hover-box" id="show-events">
+        <div class="content">
+          <h2>Show Events</h2>
+          <p>See Status and Upcoming Events Report</p>
+
+        </div>
       </div>
-    </div>
-</button>
-<button class="bttn">
-    <div class="box hover-box" id="new-faculty">
-      <div class="content">
-        <h2>New Faculty</h2>
-        <p>Welcome Admin</p>
-        
-      </div>
-    </div>
-</button>
-<button class="bttn">
-    <div class="box hover-box" id="show-events">
-      <div class="content">
-        <h2>Show Events</h2>
-        <p>See Status and Upcoming Events</p>
-        
-      </div>
-    </div>
-</button>
+    </button>
   </div>
 </body>
+
 </html>
