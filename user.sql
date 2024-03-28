349f41986f7aba5b276ee37390602b451cd22758
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-03-28 12:39:04 +0530

    New table created in database to add activities

--- user.sql
+++ user.sql
@@ -11,3 +11,15 @@ CREATE TABLE IF NOT EXISTS `login`(
 INSERT INTO `login` (`desig`,`name`,`dept`,`username`, `password`) VALUES ('Principle','Dr Vivek Kapoor','Ghriet','admin', 'admin111');
 INSERT INTO `login` (`desig`.`name`,`dept`,`username`, `password`) VALUES ('HOD','Sudhir Juare','Management','sudhir', 'sudhir11');
 INSERT INTO `login` (`desig`.`name`,`dept`,`username`, `password`) VALUES ('Faculty','Dr. Dhiraj Shembekar','Management','dhiraj', 'dhiraj11');
+
+CREATE TABLE  IF NOT EXISTS `activity` (
+    `name` VARCHAR(30),
+    `description` TEXT,
+    `datefrom` DATE,
+    `dateto`  DATE,
+    `place` VARCHAR(30),
+    `time` TIME,
+    `orgby` VARCHAR(30),
+    `sendtohod` VARCHAR(10),
+    `dept`  VARCHAR(30),
+);
