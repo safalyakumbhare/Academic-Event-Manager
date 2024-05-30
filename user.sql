1b74d4cbc1e6404219376095b3e2beb9f650f844
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-05-30 23:58:23 +0530

    bug fixed

--- user.sql
+++ user.sql
@@ -9,8 +9,8 @@ CREATE TABLE IF NOT EXISTS `login`(
 );
 
 INSERT INTO `login` (`desig`,`name`,`dept`,`username`, `password`) VALUES ('Principle','Dr Vivek Kapoor','Ghriet','admin', 'admin111');
-INSERT INTO `login` (`desig`.`name`,`dept`,`username`, `password`) VALUES ('HOD','Sudhir Juare','Management','sudhir', 'sudhir11');
-INSERT INTO `login` (`desig`.`name`,`dept`,`username`, `password`) VALUES ('Faculty','Dr. Dhiraj Shembekar','Management','dhiraj', 'dhiraj11');
+INSERT INTO `login` (`desig`,`name`,`dept`,`username`, `password`) VALUES ('HOD','Sudhir Juare','Management','sudhir', 'sudhir11');
+INSERT INTO `login` (`desig`,`name`,`dept`,`username`, `password`) VALUES ('Faculty','Dr. Dhiraj Shembekar','Management','dhiraj', 'dhiraj11');
 
 CREATE TABLE  IF NOT EXISTS `activity` (
     `name` VARCHAR(30),
